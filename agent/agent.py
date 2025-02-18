dd535a40050eb32abbee1573cde3b769b42da055
Vlad Frolov <frolvlad@gmail.com>
2025-02-18 20:59:34 +0100

    end of livestream: https://www.youtube.com/watch?v=BguBWnc2AHg

--- agent/agent.py
+++ agent/agent.py
@@ -20,9 +20,23 @@ def run(env: Environment):
     _, devpost_profile_link, target_account_id = result.split("\n")
 
     # TODO: verify the devpost link
+    if not devpost_profile_link.startswith("https://devpost.com/"):
+        env.add_reply("The provided link is not a DevPost profile link.")
+        return
+
+    devpost_profile_username = devpost_profile_link[len("https://devpost.com/"):]
+
+    devpost_profile_html = requests.get(devpost_profile_link).text
+    if "software portfolio | Devpost</title>" not in devpost_profile_html:
+        env.add_reply("The provided link is not a DevPost profile link.")
+        return
+
+    if '<span class="cp-tag">near.ai</span>' not in devpost_profile_html:
+        env.add_reply("The provided DevPost profile does not have near.ai skill tag! Please add it to your profile.")
+        return
 
     faucet_account = env.set_near("1t-agents-fund.frol.near", env.env_vars["PRIVATE_ACCESS_KEY"])
-    result = asyncio.run(faucet_account.call("1t-agents-fund.frol.near", "fund", args={"account_id": target_account_id}))
+    result = asyncio.run(faucet_account.call("1t-agents-fund.frol.near", "fund", args={"devpost_profile_username": devpost_profile_username, "account_id": target_account_id}))
 
     env.add_reply(f"You were funded! {result}")
 
--- contract/contract.py
+++ contract/contract.py
@@ -3,11 +3,19 @@ import json
 
 @near.export
 def fund():
-    args = json.loads(near.input())
-    target_account_id = args["account_id"]
-    if target_account_id != 'frol.near':
-        near.panic_str("Only frol.near can be funded")
+    if near.predecessor_account_id() != near.current_account_id():
+        near.panic_str("Unauthorized")
         return
+
+    args = json.loads(near.input())
+    devpost_profile_username = args["devpost_profile_username"]
+    target_account_id = args["account_id"]
+
+    if near.storage_has_key(devpost_profile_username):
+        near.panic_str("Already funded")
+        return
+    near.storage_write(devpost_profile_username, target_account_id)
+
     promise = near.promise_batch_create(target_account_id)
     near.promise_batch_action_transfer(promise, 5 * 10**24)
     near.promise_return(promise)
